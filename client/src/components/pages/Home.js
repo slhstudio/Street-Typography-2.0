fc3343d0905f9a7ccd82dbd52dfcf90610e0e271
slhstudio <slhstudio@gmail.com>
2021-01-01 18:46:33 -0800

    get prettier working

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,23 +1,12 @@
-import { useContext} from 'react'
+import { useContext } from 'react'
 import { PhotoContext } from '../../contexts/photoContext'
-import Loading from '../atoms/Loading';
-import PhotoGrid from '../organisms/PhotoGrid';
+import Loading from '../atoms/Loading'
+import PhotoGrid from '../organisms/PhotoGrid'
 
 const Home = () => {
-	const { loading, error } = useContext(PhotoContext)
-	
-	return (
-		<div>
-			{loading 
-			? <Loading /> 
-			: error 
-				? <div>{error}</div> 
-				: <PhotoGrid />}
-		</div>
-	)
+  const { loading, error } = useContext(PhotoContext)
+
+  return <div>{loading ? <Loading /> : error ? <div>{error}</div> : <PhotoGrid />}</div>
 }
 
-
 export default Home
-
-
